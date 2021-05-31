33ee38f2487247da3eb500f17f6110213a066228
Hana Stupková <hstupkova@gmail.com>
2021-05-31 19:05:43 +0200

    pairs: data display (without mixing)

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './style.css';
+
+const Card = ({text}) => (
+  <div className="card">{text}</div>
+);
+
+export default Card;
--- src/components/Pairs/index.jsx
+++ src/components/Pairs/index.jsx
@@ -1,12 +1,52 @@
 import React from 'react';
 import './style.css';
 
-const Pairs = () => {
+import Card from '../Card';
+import { pairs } from './pairs.js';
 
+const Pairs = () => {
+  let numberOfPairs = 9;
+  const mq = window.matchMedia('(min-width: 576px)');
+  if (mq.matches) { numberOfPairs = 12; }
+  
+  const pairsToDisplay = pairs.slice(0, numberOfPairs);
+  const wordsToDisplayCS = pairsToDisplay.map(item => { return {word: item.cs, couple: item.en}});
+  const wordsToDisplayEN = pairsToDisplay.map(item => { return {word: item.en, couple: item.cs}});
+  const wordsToDisplay = [].concat(wordsToDisplayCS, wordsToDisplayEN);
 
   return (
-    <>
-    </>
+    <main className="pairs">
+      <section className="pairs__text">
+        <h1 className="heading">Hledání dvojic</h1>
+        <p className="text">Hledej kartičky, které k sobě patří! Na některých kartičkách jsou anglické termíny, na jiných jejich české překlady. Označ, které dvě označují tu samou věc, a sbírej body. Kliknutím na tlačítko Nová hra si nech kartičky rozdat znovu. Good luck!</p>
+      </section>
+
+      <section className="pairs__wrapper">
+        <div className="game__wrapper">
+          <div className="game__button-wrapper">
+            <button className="button">Nová hra</button>
+          </div>
+
+          <div className="game">
+            {
+              wordsToDisplay.map((item, index) => 
+                <Card key={index} text={item.word} />)
+            }
+            
+          </div>
+        </div>
+        <div className="deck__wrapper">
+          <h2 className="deck__title">Nalezené dvojice</h2>
+          <div className="deck">
+            {
+              wordsToDisplay.map((item, index) => 
+                <Card key={index} text={item.word} />)
+            }
+          </div>
+        </div>
+      </section>
+
+    </main>
   );
 };
 
