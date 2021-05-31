59c472fa8d0ed9f57f08e5a0de4611abe984b05e
Eva Janoskova <janoskova.evca@gmail.com>
2021-05-31 15:55:17 +0200

    text about the project added

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -5,31 +5,45 @@ const About = () => (
   <main class="main">
     <h1 className="heading">O projektu</h1>
     <p className="text">
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure odio nisi veniam, minus soluta nemo laudantium odit reiciendis aliquid nobis officia necessitatibus ducimus impedit doloribus inventore pariatur ut quidem quas.
-      Veritatis fuga itaque voluptas voluptates commodi libero laboriosam mollitia quia. Blanditiis nisi obcaecati repellendus ipsam dolores dolor iusto aut in. Eligendi optio modi quas deserunt dolore explicabo debitis earum unde.
-      Soluta vitae reprehenderit dolorum nisi incidunt fugit ipsa tempora reiciendis iste, quas rem velit excepturi, itaque cupiditate vel numquam dolore corporis natus ex? Omnis quibusdam perspiciatis et, excepturi iusto ipsam?
+      Projekt Frontend English má za cíl podpořit studentky Digitální akademie
+      Czechitas, které se chtějí stát front-end web vývojářkami, na jejich cestě
+      za vysněnou prací. Nabízená cvičení umožňují zlepšit si slovní zásobu a
+      zároveň poskytují možnost si i otestovat základní znalosti z UX, HTML,
+      CSS, JavaScriptu a Reactu.
+    </p>
+    <p className="text">
+      Tato webová aplikace vznikla jako závěrečný projekt jarního běhu Digitální
+      akademie WEB v roce 2021 pod dohledem mentora Petra Heraleckého. Bližší
+      informace o autorkách naleznete níže.
     </p>
     <h2 className="subheading">Eva Jánošková</h2>
     <p className="text">
-      Webovou aplikací na osvojení anglických slovíček propojuji své dva
-      zájmy. Na kurzech Czechitas se ráda vzdělávám v oblasti IT. Zároveň
-      můj velký koníček je i studium cizích jazyků, jež mě momentálně živí a
-      jejichž znalost ve svém volném čase s radostí předávám dál.
+      Webovou aplikací na osvojení anglických slovíček propojuji své dva zájmy.
+      Na kurzech Czechitas se ráda vzdělávám v oblasti IT. Zároveň můj velký
+      koníček je i studium cizích jazyků, jež mě momentálně živí a jejichž
+      znalost ve svém volném čase s radostí předávám dál.
     </p>
     <p className="text">
-      Vzhledem k tomu, že nás COVID-19 uvěznil v našich domovech, jsem
-      nesmírně vděčná, že si toto období mohu zpříjemnit v komunitě
-      Czechitas a zároveň se naučit mnoho nového. Věřím, že se tímto
-      projektem přiblížím o krok blíž mému snu pracovat v IT a zároveň
-      pomocí této aplikace podpořím své spolužačky i v jejich úsílí za
-      vysněnou prací.
+      Vzhledem k tomu, že nás COVID-19 uvěznil v našich domovech, jsem nesmírně
+      vděčná, že si toto období mohu zpříjemnit v komunitě Czechitas a zároveň
+      se naučit mnoho nového. Věřím, že se tímto projektem přiblížím o krok blíž
+      mému snu pracovat v IT a zároveň pomocí této aplikace podpořím své
+      spolužačky i v jejich úsílí za vysněnou prací.
     </p>
     <h2 className="subheading">Hana Stupková</h2>
     <p className="text">
-      Vystudovala jsem kartografii a geoinformatiku a po škole pracovala v IT firmě; nejprve v různých GIS, poté se ze mě stal analytik (což zahrnovalo jak práci s datovými modely a zadávání úkolů pro programátory, tak třeba i analýzu a psaní dokumentů pro různé úřady ve státní správě).
+      Vystudovala jsem kartografii a geoinformatiku a po škole pracovala v IT
+      firmě; nejprve v různých GIS, poté se ze mě stal analytik (což zahrnovalo
+      jak práci s datovými modely a zadávání úkolů pro programátory, tak třeba i
+      analýzu a psaní dokumentů pro různé úřady ve státní správě).
     </p>
     <p className="text">
-      Zhruba před rokem a půl jsem objevila Czechitas a covidové období využila k rozšiřování si znalostí v oblasti IT (jestli dobře počítám, absolvovala jsem už 9 kurzů na různá témata). Zlomem byl kurz Staň se kódérkou, kde jsem objevila front-end a to, že mě kódování velmi baví. V digitální akademii jsem se chtěla naučit JavaScript a React a jejich znalost si vyzkoušet při kódování nějakého pěkného interaktivního projektu.
+      Zhruba před rokem a půl jsem objevila Czechitas a covidové období využila
+      k rozšiřování si znalostí v oblasti IT (jestli dobře počítám, absolvovala
+      jsem už 9 kurzů na různá témata). Zlomem byl kurz Staň se kódérkou, kde
+      jsem objevila front-end a to, že mě kódování velmi baví. V digitální
+      akademii jsem se chtěla naučit JavaScript a React a jejich znalost si
+      vyzkoušet při kódování nějakého pěkného interaktivního projektu.
     </p>
   </main>
 );
