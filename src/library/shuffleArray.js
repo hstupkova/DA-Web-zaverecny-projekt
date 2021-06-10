b04297067e2c02b38f9261693fbf32063c56c46c
Hana Stupková <hstupkova@gmail.com>
2021-06-10 15:26:27 +0200

    shuffleArray() fixed, menu items focus removed

--- src/library/shuffleArray.js
+++ src/library/shuffleArray.js
@@ -1,4 +1,6 @@
-export const shuffleArray = (array) => {
+export const shuffleArray = (originalArray) => {
+  let array = [...originalArray];
+  
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
